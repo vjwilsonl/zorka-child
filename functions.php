c34b04fc5768d1880f8a7d4c299dd57625db75ae
vjwilsonl <vjwilsonl@gmail.com>
2018-10-30 15:29:21 +0800

    Add out of stock on product

--- functions.php
+++ functions.php
@@ -5,4 +5,11 @@ function child_theme_enqueue_styles() {
 }
 
 // if you want to add some custom function
+// display an 'Out of Stock' label on archive pages
+add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_stock', 10 );
+function woocommerce_template_loop_stock() {
+    global $product;
+    if ( ! $product->managing_stock() && ! $product->is_in_stock() )
+        echo '<p class="stock out-of-stock">Out of Stock</p>';
+}
 ?>
