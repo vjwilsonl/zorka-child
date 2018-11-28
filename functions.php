e16d11d5f7beea7e5341ad7227ec71da0ccb2fd6
vjwilsonl <vjwilsonl@gmail.com>
2018-11-28 16:08:10 +0800

    confirm password

--- functions.php
+++ functions.php
@@ -12,4 +12,49 @@ function woocommerce_template_loop_stock() {
     if ( ! $product->managing_stock() && ! $product->is_in_stock() )
         echo '<p class="stock out-of-stock">Out of Stock</p>';
 }
+
+// Add a second password field to the checkout page in WC 3.x.
+add_filter( 'woocommerce_checkout_fields', 'wc_add_confirm_password_checkout', 10, 1 );
+function wc_add_confirm_password_checkout( $checkout_fields ) {
+    if ( get_option( 'woocommerce_registration_generate_password' ) == 'no' ) {
+        $checkout_fields['account']['account_password2'] = array(
+                'type'              => 'password',
+                'label'             => __( 'Confirm password', 'woocommerce' ),
+                'required'          => true,
+                'placeholder'       => _x( 'Confirm Password', 'placeholder', 'woocommerce' )
+        );
+    }
+
+    return $checkout_fields;
+}
+
+// Check the password and confirm password fields match before allow checkout to proceed.
+add_action( 'woocommerce_after_checkout_validation', 'wc_check_confirm_password_matches_checkout', 10, 2 );
+function wc_check_confirm_password_matches_checkout( $posted ) {
+    $checkout = WC()->checkout;
+    if ( ! is_user_logged_in() && ( $checkout->must_create_account || ! empty( $posted['createaccount'] ) ) ) {
+        if ( strcmp( $posted['account_password'], $posted['account_password2'] ) !== 0 ) {
+            wc_add_notice( __( 'Passwords do not match.', 'woocommerce' ), 'error' );
+        }
+    }
+}
+// Add the code below to your theme's functions.php file to add a confirm password field on the register form under My Accounts.
+add_filter('woocommerce_registration_errors', 'registration_errors_validation', 10,3);
+function registration_errors_validation($reg_errors, $sanitized_user_login, $user_email) {
+    global $woocommerce;
+    extract( $_POST );
+    if ( strcmp( $password, $password2 ) !== 0 ) {
+        return new WP_Error( 'registration-error', __( 'Passwords do not match.', 'woocommerce' ) );
+    }
+    return $reg_errors;
+}
+add_action( 'woocommerce_register_form', 'wc_register_form_password_repeat' );
+function wc_register_form_password_repeat() {
+    ?>
+    <p class="form-row form-row-wide">
+        <label for="reg_password2"><?php _e( 'Password Repeat', 'woocommerce' ); ?> <span class="required">*</span></label>
+        <input type="password" class="input-text" name="password2" id="reg_password2" value="<?php if ( ! empty( $_POST['password2'] ) ) echo esc_attr( $_POST['password2'] ); ?>" />
+    </p>
+    <?php
+}
 ?>
